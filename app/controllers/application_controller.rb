2ff39ded61645bfd6a831db25919adcb91b00992
yosukefujii0222 <look.in.my.eyes@softbank.ne.jp>
2017-09-13 12:35:58 +0900

    Delete previous commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_params, if: :devise_controller?
-
-  def configure_permitted_params
-    devise_parameter_sanitizer.for(:sign_up) {
-      |u| u.permit(:email, :password, :password_confirmation, :name)
-    }
-  end
-
   protect_from_forgery with: :exception
 end
