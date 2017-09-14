87af91bf3d0c621b1a113bdbfc8560e6a11fc16f
yosukefujii0222 <look.in.my.eyes@softbank.ne.jp>
2017-09-14 21:49:20 +0900

    Create new group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,12 +1,20 @@
 class GroupsController < ApplicationController
   def new
-    @group = Menmber.new
+    @group = Group.new
   end
 
   def create
+    Group.create(strong_params)
+    redirect_to :root
   end
 
   def edit
+  end
+
+  private
+
+  def strong_params
+    params.require(:group).permit(:groupname, user_ids: [])
   end
 
 end
