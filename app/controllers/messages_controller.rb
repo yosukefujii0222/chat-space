5b0bc54ef5856930f9509d17c4e86596bf946e7b
yosukefujii0222 <look.in.my.eyes@softbank.ne.jp>
2017-09-23 17:54:42 +0900

    Edit controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,17 +9,19 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to group_messages_url, notice: "メッセージを送信しました"
+      respond_to do |format|
+        format.html { redirect_to group_messages_url, notice: "メッセージを送信しました" }
+        format.json
+      end
     else
       flash.now[:alert] = "メッセージを入力してください"
       render 'index'
     end
-
   end
 
   private
   def message_params
-    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
+    params.require(:message).permit(:user_name, :body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 
 end
