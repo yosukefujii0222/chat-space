c7fa859ac9bbe8dcc9da30788a1188acf4134589
yosukefujii0222 <look.in.my.eyes@softbank.ne.jp>
2017-09-26 12:19:43 +0900

    Ajax and auto_scroll

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,13 +1,25 @@
 $(function() {
-  function buildHTML(message) {
+  //メッセージと画像どちらもありの場合
+  function body_and_image(message) {
     var html = `<div class= "chat-main__body_list--message-name">${message.user_name}</div>
                 <div class= "chat-main__body_list--message-time">${message.updated_at}</div>
                 <div class= "clear"></div>
                 <div class= "chat-main__body_list--message-body">${message.body}</div>
-                  <% if message.image? %>
-                    <%= image_tag message.image_url(:thumb) %>
-                  <% end %>`
+                <div class= "chat-main__body_list--message-image" src="${message.image}"></div>`;
     return html;
+  }
+  //メッセージだけの場合
+  function only_body(message) {
+    var html = `<div class= "chat-main__body_list--message-name">${message.user_name}</div>
+                <div class= "chat-main__body_list--message-time">${message.updated_at}</div>
+                <div class= "clear"></div>
+                <div class= "chat-main__body_list--message-body">${message.body}</div>`;
+    return html;
+  }
+  function auto_scroll() {
+    var scroll_target = $('.scroll_target').offset().top;
+    $(".chat-main__body").animate({scrollTop:scroll_target});
+    return false;
   }
   $('.new_message').on('submit', function(e) {
     e.preventDefault();
@@ -22,18 +34,25 @@ $(function() {
       contentType: false,
     })
     .done(function(data) {
-      var html = buildHTML(data);
-      console.log(html)
-      $('.chat-main__body_list--message').append(html)
-      $('.new_message').val('')
+      if(data.image == null) {
+        var html = only_body(data);
+        $('.chat-main__body_list--message').append(html);
+        $('.new_message').val('');
+        $('.send-button').prop('disabled', false);
+        auto_scroll();
+      }else {
+        var html = body_and_image(data);
+        $('.chat-main__body_list--message').append(html);
+        $('.new_message').val('');
+        $('.send-button').prop('disabled', false);
+        auto_scroll();
+      }
     })
     .fail(function() {
       alert('error');
     })
-  })
+  });
+
 });
-
-
-
 
 
