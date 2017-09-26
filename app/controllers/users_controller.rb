8e882a0f746f14946a718589f94a19141a3fde08
yosukefujii0222 <look.in.my.eyes@softbank.ne.jp>
2017-09-26 15:38:17 +0900

    Add index-action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.jason
+    end
+  end
+
   def edit
   end
 
   def update
   end
-
 end
